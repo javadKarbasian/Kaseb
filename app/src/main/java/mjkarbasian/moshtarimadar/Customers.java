0bcb67f3a8c7837a7de44042748216c1c4c197ec
javadkarbasian <javad.karbasian@gmail.com>
2016-06-24 17:51:47 +0430

    finish customer list going to add debators list

--- app/src/main/java/mjkarbasian/moshtarimadar/Customers.java
+++ app/src/main/java/mjkarbasian/moshtarimadar/Customers.java
@@ -1,13 +1,20 @@
 package mjkarbasian.moshtarimadar;
 
+import android.app.SearchManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class Customers extends DrawerActivity {
 
@@ -24,7 +31,33 @@ public class Customers extends DrawerActivity {
 
     public void fab_customers(View v){
         Snackbar.make(v, "Replace your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                .setAction("Action", null).show();
+        PopupMenu popup = new PopupMenu(this,v);
+        popup.getMenu().add(R.string.fab_add_customer);
+        popup.getMenu().add(R.string.fab_add_sale);
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+        popup.show();//showing popup menu
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_customers, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(this.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search_button).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        return true;
     }
 
 }
