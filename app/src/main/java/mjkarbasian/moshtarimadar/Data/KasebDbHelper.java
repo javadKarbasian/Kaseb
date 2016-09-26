4f6b57952efabf65e9099b5c7f9c8f4abf97c210
javadkarbasian <javad.karbasian@gmail.com>
2016-09-26 05:55:04 +0330

    add state,sale,detailsale table in kasebdbhelper, must add other tables

--- app/src/main/java/mjkarbasian/moshtarimadar/Data/KasebDbHelper.java
+++ app/src/main/java/mjkarbasian/moshtarimadar/Data/KasebDbHelper.java
@@ -27,7 +27,7 @@ public class KasebDbHelper extends SQLiteOpenHelper {
                 KasebContract.Customers.COLUMN_LAST_NAME + " TEXT NOT NULL," +
                 KasebContract.Customers.COLUMN_PHONE_MOBILE + " TEXT NOT NULL UNIQUE,"+
                 " FOREIGN KEY ("+ KasebContract.Customers.COLUMN_STATE_ID +") REFERENCES "+
-                KasebContract.State.TABLE_NAME + " ("+KasebContract.State._ID+") " +
+                KasebContract.State.TABLE_NAME + " ("+KasebContract.State._ID+"), " +
                 KasebContract.Customers.COLUMN_PHONE_WORK + " TEXT,"+
                 KasebContract.Customers.COLUMN_PHONE_FAX + " TEXT," +
                 KasebContract.Customers.COLUMN_PHONE_OTHER+ " TEXT,"+
@@ -38,7 +38,35 @@ public class KasebDbHelper extends SQLiteOpenHelper {
                 KasebContract.Customers.COLUMN_ADDRESS_STREET+ " TEXT,"+
                 KasebContract.Customers.COLUMN_ADDRESS_POSTAL_CODE+ " TEXT,"+
                 KasebContract.Customers.COLUMN_DESCRIPTION+" TEXT,"+
-                KasebContract.Customers.COLUMN_IS_DELETED+ " BLOB," + ");";
+                KasebContract.Customers.COLUMN_IS_DELETED+ " BLOB NOT NULL," + ");";
+        //creating state Table
+        final String CREATE_STATE_TABLE = "CREATE TABLE " + KasebContract.State.TABLE_NAME + "(" +
+                KasebContract.State._ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                KasebContract.State.COLUMN_STATE_POINTER + " INTEGER NOT NULL" + ");";
+        //creating sales table
+        final String CREATE_SALES_TABLE = "CREATE TABLE " + KasebContract.Sales.TABLE_NAME + "(" +
+                KasebContract.Sales._ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                KasebContract.Sales.COLUMN_SALE_CODE + " TEXT NOT NULL," +
+                " FOREIGN KEY ("+KasebContract.Sales.COLUMN_CUSTOMER_ID+") REFERENCES "+
+                KasebContract.Customers.TABLE_NAME + " ("+KasebContract.Customers._ID+"), "+
+                KasebContract.Sales.COLUMN_IS_DELETED + " BLOB NOT NULL," +
+                ");";
+        //creating detail sale taBLE
+        final String CREATE_DETAIL_SALE_TABLE = "CREATE TABLE " + KasebContract.DetailSale.TABLE_NAME+ "(" +
+                KasebContract.DetailSale._ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " FOREIGN KEY (" + KasebContract.DetailSale.COLUMN_SALE_ID + ") REFERENCES "+
+                KasebContract.Sales.TABLE_NAME + " ("+ KasebContract.Sales._ID + "), "+
+                KasebContract.DetailSale.COLUMN_DATE + " TEXT NOT NULL," +
+                KasebContract.DetailSale.COLUMN_ITEMS_NUMBER+ " INTEGER NOT NULL," +
+                KasebContract.DetailSale.COLUMN_SUB_TOTAL + " REAL NOT NULL," +
+                KasebContract.DetailSale.COLUMN_TOTAL_DISCOUNT + " REAL NOT NULL," +
+                KasebContract.DetailSale.COLUMN_TOTAL_TAX + " REAL NOT NULL,"+
+                KasebContract.DetailSale.COLUMN_TOTAL_DUE + " REAL NOT NULL,"+
+                KasebContract.DetailSale.COLUMN_TOTAL_PAID+ " REAL NOT NULL,"+
+                KasebContract.DetailSale.COLUMN_IS_BALANCED+ " BLOB NOT NULL"+
+                ");";
+        //create detail sale payment table
+
     }
 
     @Override
