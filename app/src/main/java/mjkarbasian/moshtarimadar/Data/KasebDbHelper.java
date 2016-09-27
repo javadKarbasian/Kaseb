f32cb87f8d358c8c67d98160b9d8a95c24d914e3
javadkarbasian <javad.karbasian@gmail.com>
2016-09-27 06:01:06 +0330

    add detailSale _payment & _product & _taxes table must add others

--- app/src/main/java/mjkarbasian/moshtarimadar/Data/KasebDbHelper.java
+++ app/src/main/java/mjkarbasian/moshtarimadar/Data/KasebDbHelper.java
@@ -38,7 +38,7 @@ public class KasebDbHelper extends SQLiteOpenHelper {
                 KasebContract.Customers.COLUMN_ADDRESS_STREET+ " TEXT,"+
                 KasebContract.Customers.COLUMN_ADDRESS_POSTAL_CODE+ " TEXT,"+
                 KasebContract.Customers.COLUMN_DESCRIPTION+" TEXT,"+
-                KasebContract.Customers.COLUMN_IS_DELETED+ " BLOB NOT NULL," + ");";
+                KasebContract.Customers.COLUMN_IS_DELETED+ " BLOB NOT NULL" + ");";
         //creating state Table
         final String CREATE_STATE_TABLE = "CREATE TABLE " + KasebContract.State.TABLE_NAME + "(" +
                 KasebContract.State._ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
@@ -49,7 +49,7 @@ public class KasebDbHelper extends SQLiteOpenHelper {
                 KasebContract.Sales.COLUMN_SALE_CODE + " TEXT NOT NULL," +
                 " FOREIGN KEY ("+KasebContract.Sales.COLUMN_CUSTOMER_ID+") REFERENCES "+
                 KasebContract.Customers.TABLE_NAME + " ("+KasebContract.Customers._ID+"), "+
-                KasebContract.Sales.COLUMN_IS_DELETED + " BLOB NOT NULL," +
+                KasebContract.Sales.COLUMN_IS_DELETED + " BLOB NOT NULL" +
                 ");";
         //creating detail sale taBLE
         final String CREATE_DETAIL_SALE_TABLE = "CREATE TABLE " + KasebContract.DetailSale.TABLE_NAME+ "(" +
@@ -66,6 +66,33 @@ public class KasebDbHelper extends SQLiteOpenHelper {
                 KasebContract.DetailSale.COLUMN_IS_BALANCED+ " BLOB NOT NULL"+
                 ");";
         //create detail sale payment table
+        final String CREATE_DETAIL_SALE_PAYMENT_TABLE = "CREATE TABLE " + KasebContract.DetailSalePayments.TABLE_NAME+ "(" +
+                KasebContract.DetailSalePayments._ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " FOREIGN KEY (" + KasebContract.DetailSalePayments.COLUMN_DETAIL_SALE_ID + ") REFERENCES "  +
+                KasebContract.DetailSale.TABLE_NAME + " ("+ KasebContract.DetailSale._ID + "), " +
+                " FOREIGN KEY (" + KasebContract.DetailSalePayments.COLUMN_PAYMENT_METHOD_ID + ") REFERENCES "  +
+                KasebContract.PaymentMethods.TABLE_NAME + " ("+ KasebContract.PaymentMethods._ID + "), " +
+                KasebContract.DetailSalePayments.COLUMN_DUE_DATE + " TEXT NOT NULL," +
+                KasebContract.DetailSalePayments.COLUMN_AMOUNT + " REAL NOT NULL" + ");";
+        //create detail sale product table
+        final String CREATE_DETAIL_SALE_PRODUCT_TABLE = "CREATE TABLE " + KasebContract.DetailSaleProducts.TABLE_NAME+ "(" +
+                KasebContract.DetailSaleProducts._ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " FOREIGN KEY (" + KasebContract.DetailSaleProducts.COLUMN_DETAIL_SALE_ID + ") REFERENCES "  +
+                KasebContract.DetailSale.TABLE_NAME + " ("+ KasebContract.DetailSale._ID + "), " +
+                " FOREIGN KEY (" + KasebContract.DetailSaleProducts.COLUMN_PRODUCT_ID + ") REFERENCES "  +
+                KasebContract.Products.TABLE_NAME + " ("+ KasebContract.Products._ID + "), " +
+                KasebContract.DetailSaleProducts.COLUMN_QUANTITY + " INTEGER NOT NULL," +
+                KasebContract.DetailSaleProducts.COLUMN_AMOUNT + " REAL NOT NULL" + ");";
+        //create detail sale taxes table
+        final String CREATE_DETAIL_SALE_TAXES_TABLE = "CREATE TABLE " + KasebContract.DetailSaleTaxes.TABLE_NAME+ "(" +
+                KasebContract.DetailSaleTaxes._ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " FOREIGN KEY (" + KasebContract.DetailSaleTaxes.COLUMN_DETAIL_SALE_ID + ") REFERENCES "  +
+                KasebContract.DetailSale.TABLE_NAME + " ("+ KasebContract.DetailSale._ID + "), " +
+                " FOREIGN KEY (" + KasebContract.DetailSaleTaxes.COLUMN_TAX_TYPE_ID + ") REFERENCES "  +
+                KasebContract.TaxTypes.TABLE_NAME + " ("+ KasebContract.TaxTypes._ID + "), " +
+                KasebContract.DetailSaleTaxes.COLUMN_AMOUNT + " REAL NOT NULL" + ");";
+
+
 
     }
 
