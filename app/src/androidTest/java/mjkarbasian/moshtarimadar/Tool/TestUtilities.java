8f65d7a2f1aa18c750b8e482069ff43bf876b2b5
morteza <milad7111@chmail.ir>
2016-10-04 01:39:13 +0330

    add some code in TestDb & add TestUtilities class without code, must complete the code in test classes

---
+++ app/src/androidTest/java/mjkarbasian/moshtarimadar/Tool/TestUtilities.java
@@ -0,0 +1,9 @@
+package mjkarbasian.moshtarimadar.Tool;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Unique on 10/4/2016.
+ */
+public class TestUtilities extends AndroidTestCase {
+}
